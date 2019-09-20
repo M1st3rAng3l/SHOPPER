ee8e46240b5eeff18804c64ffb7c940519625ec2
Angel Rodriguez Herrera <42501960+M1st3rAng3l@users.noreply.github.com>
2019-09-20 16:56:03 -0500

    header version 1.2

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,48 @@
-import React from 'react';
-import logo1 from '../imagenes/Cart icon.svg'
-import logo2 from '../imagenes/shortlist icon.svg'
+import React from "react";
+import "../css/header.css";
+import logo1 from "../imagenes/Cart icon.svg";
+import logo2 from "../imagenes/shortlist icon.svg";
 
+function HeaderOne() {
+  return (
+    <div>
+      <div className="headerBox">
 
-function HeaderOne(){
-    return(
-        <div>
-        <div>
-           <h1>SHOPPER</h1>
-           <h2>Smart Marketplace</h2>
+        <div className="titleBox">
+          <h1>SHOPPER</h1>
+          <p>Smart Marketplace</p>
         </div>
 
-        <div>
-            <input type="text" placeholder="buscar productos"/>
-            <button type="submit">BUSCAR</button>
+        <div className="buscadorBox">
+          <input type="text" placeholder="buscar productos" />
+          <button type="submit">BUSCAR</button>
         </div>
-        
-        <div>
-            <button>INICIAR SESION</button>
-            <button>REGISTRARSE</button>
+
+        <div className="botonBox">
+          <button>INICIAR SESION</button>
+          <button>REGISTRARSE</button>
         </div>
-        <div>
-            <img src={logo2} alt="icon1"/>
-            <img src={logo1} alt="logo2"/>
+        <div className="imagenBox">
+          <img src={logo2} alt="icon1" />
+          <img src={logo1} alt="logo2" />
         </div>
-        </div>
-    )
+      </div>
+
+      <nav className="navbarBox">
+        <ul>
+          <li>Decoración del hogar</li>
+          <li>Muebles</li>
+          <li>Luces</li>
+          <li>Acentos caseros</li>
+          <li>Alfombras</li>
+          <li>Exteriores</li>
+          <li>Vacaiones</li>
+          <li>Regalos</li>
+          <li>Eventos</li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default HeaderOne;
