6ebd58d1a72d9ac7d1084e2ee04a6476887b8bcf
Angel Rodriguez Herrera <42501960+M1st3rAng3l@users.noreply.github.com>
2019-09-20 15:40:28 -0500

    header version 1.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import HEADER from './components/header'
-import './App.css';
 
 function App() {
   return (
--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import logo1 from '../imagenes/Cart icon.svg'
+import logo2 from '../imagenes/shortlist icon.svg'
+
 
 function HeaderOne(){
     return(
         <div>
         <div>
-            <img src="" alt=""></img>
+           <h1>SHOPPER</h1>
+           <h2>Smart Marketplace</h2>
         </div>
 
         <div>
             <input type="text" placeholder="buscar productos"/>
             <button type="submit">BUSCAR</button>
+        </div>
+        
+        <div>
+            <button>INICIAR SESION</button>
+            <button>REGISTRARSE</button>
+        </div>
+        <div>
+            <img src={logo2} alt="icon1"/>
+            <img src={logo1} alt="logo2"/>
         </div>
         </div>
     )
