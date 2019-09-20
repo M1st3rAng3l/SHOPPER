2ad3f46e9ccb3c3afb064417fd57eb548584add9
Angel Rodriguez Herrera <42501960+M1st3rAng3l@users.noreply.github.com>
2019-09-20 15:02:43 -0500

    header version 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HEADER from './components/header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HEADER/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function HeaderOne(){
+    return(
+        <div>
+        <div>
+            <img src="" alt=""></img>
+        </div>
+
+        <div>
+            <input type="text" placeholder="buscar productos"/>
+            <button type="submit">BUSCAR</button>
+        </div>
+        </div>
+    )
+}
+
+export default HeaderOne;
