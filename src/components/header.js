e386ed02752c4ac236244eac0381984ced55f8e8
Angel Rodriguez Herrera <42501960+M1st3rAng3l@users.noreply.github.com>
2019-09-23 10:14:26 -0500

    header finalizado

--- src/components/header.js
+++ src/components/header.js
@@ -9,40 +9,38 @@ function HeaderOne() {
       <div className="headerBox">
 
         <div className="titleBox">
-          <h1>SHOPPER</h1>
-          <p>Smart Marketplace</p>
+          <h1 className="titleOne"><strong>SH<span>O</span>PPER</strong></h1>
+          <h3 className="titleTwo">Smart Marketplace</h3>
         </div>
 
-        <div className="buscadorBox">
-          <input type="text" placeholder="buscar productos" />
-          <button type="submit">BUSCAR</button>
+        <div className="searchBox">
+          <input type="text" placeholder="buscar productos" className="searchBar"/>
+          <button type="submit" className="searchButton">BUSCAR</button>
         </div>
 
         <div className="botonBox">
-          <button>INICIAR SESION</button>
-          <button>REGISTRARSE</button>
+          <button className="btnUP">INICIAR SESION</button>
+          <button className="btnUP">REGISTRARSE</button>
+          <img src={logo2} alt="icon1" className="imgLogo"/>
+          <img src={logo1} alt="logo2" className="imgLogo"/>
         </div>
-        <div className="imagenBox">
-          <img src={logo2} alt="icon1" />
-          <img src={logo1} alt="logo2" />
-        </div>
+
       </div>
 
       <nav className="navbarBox">
-        <ul>
-          <li>Decoración del hogar</li>
-          <li>Muebles</li>
-          <li>Luces</li>
-          <li>Acentos caseros</li>
-          <li>Alfombras</li>
-          <li>Exteriores</li>
-          <li>Vacaiones</li>
-          <li>Regalos</li>
-          <li>Eventos</li>
-        </ul>
+
+          <button className="optionButton">Decoración del hogar</button>
+          <button className="optionButton">Muebles</button>
+          <button className="optionButton">Luces</button>
+          <button className="optionButton">Acentos caseros</button>
+          <button className="optionButton">Alfombras</button>
+          <button className="optionButton">Exteriores</button>
+          <button className="optionButton">Vacaciones</button>
+          <button className="optionButton">Regalos</button>
+          <button className="optionButton">Eventos</button>
+        
       </nav>
     </div>
   );
 }
-
 export default HeaderOne;
