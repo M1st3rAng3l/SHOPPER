bb0217babbbc5a4e5f0643d7c0d794b2d6012ef5
Angel Rodriguez Herrera <42501960+M1st3rAng3l@users.noreply.github.com>
2019-09-25 16:32:30 -0500

    topseller en proceso

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,53 +1,69 @@
-import React, {useState} from 'react';
-import img1 from '../imagenes/bicicleta.png';
-import img2 from '../imagenes/joya.png';
-import img3 from '../imagenes/mueble.png';
-import img4 from '../imagenes/maquillaje.png';
-import left from '../imagenes/left.png';
-import right from '../imagenes/right.png';
-import '../css/carousel.css'
+import React, { useState } from "react";
+import img1 from "../imagenes/bicicleta.png";
+import img2 from "../imagenes/joya.png";
+import img3 from "../imagenes/mueble.png";
+import img4 from "../imagenes/maquillaje.png";
+import left from "../imagenes/left.png";
+import right from "../imagenes/right.png";
+import "../css/carousel.css";
 
-function Carousel(){
+function Carousel() {
   const images = [img1, img2, img4, img3];
   const [currentImage, setcurrentImage] = useState(0);
 
-  function prevIMage(){
+  function prevIMage() {
     //averiguar si currentImage es igual a 0 y si el usuario alcanzó el comienzo del carrusel
     const resetToVeryBack = currentImage === 0;
-    const index1 = resetToVeryBack ? images.length -1: currentImage - 1;
+    const index1 = resetToVeryBack ? images.length - 1 : currentImage - 1;
 
     // asigna el índice lógico al índice de imagen actual que se usará en el método de representación
-    setcurrentImage(index1)
+    setcurrentImage(index1);
   }
 
-  function nextImage(){
+  function nextImage() {
     // verifica si necesitamos comenzar desde el primer índice
-      const resetIndex = currentImage === images.length - 1;
-      const index2 = resetIndex ? 0 : currentImage  +1;
-      setcurrentImage(index2)
+    const resetIndex = currentImage === images.length - 1;
+    const index2 = resetIndex ? 0 : currentImage + 1;
+    setcurrentImage(index2);
   }
 
   // crea una nueva matriz con 4 elementos de las imágenes de origen
   const ImageSources = images.slice(currentImage, currentImage + 4);
 
   // verifica la longitud de la nueva matriz (es menor que 4 cuando el índice está al final de la matriz de fuentes imaginarias)
-  const imageSourcesToDisplay = ImageSources.length < 4
-  // si la longitud de imageSourcesToDisplay es inferior a 4 imágenes que añaden las imágenes que faltan desde el comienzo de la matriz original
-    ? [...ImageSources, ...images.slice(0, 4 - ImageSources.length) ]: ImageSources;
+  const imageSourcesToDisplay =
+    ImageSources.length < 4
+      ? // si la longitud de imageSourcesToDisplay es inferior a 4 imágenes que añaden las imágenes que faltan desde el comienzo de la matriz original
+        [...ImageSources, ...images.slice(0, 4 - ImageSources.length)]
+      : ImageSources;
 
   return (
     <div>
-    <div className="carouselBox">
-      <img src={left} alt="iconLeft" className="btnIcon" onClick={prevIMage}/>
-       {/* renderiza imagenes */}
-       {imageSourcesToDisplay.map((image, index) =>
-        <img key={index} src={image} alt="imagesCollection" className="imgCollection"/>
-      )}
-      <img src={right} alt='iconRight' className="btnIcon" onClick={nextImage}/>
+      <div className="carouselBox">
+        <img
+          src={left}
+          alt="iconLeft"
+          className="btnIcon"
+          onClick={prevIMage}
+        />
+        {/* renderiza imagenes */}
+        {imageSourcesToDisplay.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt="imagesCollection"
+            className="imgCollection"
+          />
+        ))}
+        <img
+          src={right}
+          alt="iconRight"
+          className="btnIcon"
+          onClick={nextImage}
+        />
       </div>
     </div>
   );
-};
+}
 
 export default Carousel;
-
--- src/components/topSeller.js
+++ src/components/topSeller.js
@@ -1,28 +1,71 @@
-import React, {useState}from 'react';
-import iconTop from '../imagenes/Top Seller.png'
+import React from "react";
+import iconTop from "../imagenes/Top Seller.png";
+import bike from "../imagenes/bicicleta.png";
+import mueble from "../imagenes/mueble2.png";
+import glass from "../imagenes/lentes.png";
+import tech from "../imagenes/tech.png";
+import "../css/topSeller.css";
 
 function TopSeller() {
+  const products = [
+    {
+      id: "01",
+      name: "MERRY´S Cat Eye Sunglasses Women",
+      price: "125.00",
+      supplier: "Colbat Union",
+      description: "Son lentes de sol para mujeres ",
+      quantity: "100",
+      imagen: glass
+    },
+    {
+      id: "02",
+      name: "SAVA DESCK300 Mountain Bike 29",
+      price: "1400.00",
+      supplier: "Cyrsshare",
+      description: "Una bicileta para ir en cualquier terreno montañosa",
+      quantity: "10",
+      imagen: bike
+    },
+    {
+      id: "03",
+      name: "Premium Wood Luna Bench",
+      price: "355.00",
+      supplier: "Cobind pvt.",
+      description: "Un bello banco para exteriores ",
+      quantity: "10",
+      imagen: mueble
+    },
+    {
+      id: "04",
+      name: "Hardware Mini",
+      price: "555.00",
+      supplier: "Intel Tech",
+      description: "Un minidispositivo",
+      quantity: "10",
+      imagen: tech
+    }
+  ];
 
-    const products = [
-        {id:'01', name:'Lentes de Sol', precio:'125.00',distribuidora: 'Colbat Union', descripcion:'Son lentes de sol para mujeres ', cantidad:'10'},
-        {id:'02', name:'Bicicleta de Montaña', precio:'1400',distribuidora: 'Colbat Union', descripcion:'Son lentes de sol para mujeres ', cantidad:'10'},
-        {id:'03', name:'Lentes de Sol', precio:'125.00',distribuidora: 'Colbat Union', descripcion:'Son lentes de sol para mujeres ', cantidad:'10'},
-        {id:'04', name:'Lentes de Sol', precio:'125.00',distribuidora: 'Colbat Union', descripcion:'Son lentes de sol para mujeres ', cantidad:'10'},
-        {id:'05', name:'Lentes de Sol', precio:'125.00',distribuidora: 'Colbat Union', descripcion:'Son lentes de sol para mujeres ', cantidad:'10'},
-    ];
-
-    const [topSold, setTopSold] = useState(0) 
-    return(
-        <div>
-            <div className="mainBox1">
-                <div>
-                    <img src={iconTop} alt="iconTitle"/>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="mainBox1">
+        <div className="">
+          <img src={iconTop} alt="iconTitle" />
         </div>
-    )
-    
+        {products.map((e, index) => (
+          <div key={index} className="dataBox">
+            <img src={e.imagen} alt="imgItem" className="imgItem" />
+            <p className="text">{e.name}</p>
+            <p className="text">${e.price}</p>
+            <p className="text">{e.supplier}</p>
+            <p className="text">{e.description}</p>
+          </div>
+        ))}
+         <p>EXPLORAR MAS</p>
+      </div>
+     
+    </div>
+  );
 }
 
 export default TopSeller;
-
